ea07b431315466aecf7bc06691f3fc30b11d868e
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-10-09 22:10:06 -0700

    updates and refactors

--- src/constants/resume_url.js
+++ src/constants/resume_url.js
@@ -1,2 +1,2 @@
 export const RESUME_URL =
-  "https://drive.google.com/file/d/1n8fMk_whirJGO-hBM9RpSsT6ZkXgnDaQ/view?usp=sharing";
+  "https://drive.google.com/file/d/1PEbXrAtvqBpUjcuN5hdHgrFnDAWKGcNI/view?usp=sharing";
