6d981f0bbb0d457d136a8b9471125fd1c05e27f6
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-10 09:34:53 -0800

    update about

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,25 +8,25 @@ export default function About({ mode }) {
         <p>
           As backend developer, I have a passion for building robust and
           scalable applications that can handle large amounts of data and
-          traffic. However, I also have experience in frontend development,
-          including HTML, CSS, and JavaScript, as well as modern frameworks like
-          React.
+          traffic. However, I also have experience in frontend development, with
+          the use of stacks like HTML, CSS, and JavaScript, TypeScript as well
+          as modern frontend frameworks like React and NextJs.
         </p>
+        <br />
         <p>
-          Things that sets me apart from other developers is my experience in
-          contributing to both backend and frontend projects. This has given me
-          a unique perspective on the development process, allowing me to
+          This sets me apart from other developers in that it has given me a
+          unique perspective on the development process, allowing me to
           collaborate seamlessly with other backend developers to provide API's
-          that meets the full need of the frontend developers enabling them
-          build applications that are not only powerful and efficient, but also
+          that meets the full need of the frontend team enabling them build
+          applications that are not only powerful and efficient, but also
           user-friendly and visually appealing.
         </p>
-
+        <br />
         <p>
           I am always looking for new challenges and opportunities to expand my
-          knowledge and skills in Python backend development.If you are looking
-          for a developer who is passionate about building high-quality,
-          scalable applications, I would love to hear from you.
+          knowledge and skills in backend development. If you are looking for a
+          developer who is passionate about building robust and scalable
+          applications, I would love to hear from you.
         </p>
       </div>
     </section>
